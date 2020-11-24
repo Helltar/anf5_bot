4eee298e58c2a4d16567c7d2318b53c48e497bce
Helltar <helltar.live@gmail.com>
2020-11-24 15:57:14 +0200

    upd. BotConfig.java

--- src/me/telegram/anf5_bot/BotConfig.java
+++ src/me/telegram/anf5_bot/BotConfig.java
@@ -4,7 +4,7 @@ public class BotConfig {
     
     public static String CHANNEL_NAME = "@anf5_dev"; // channel.txt
 
-    private static final String FILES_PATH = "/storage/emulated/0/AppProjects/anf5_bot/";
+    private static final String FILES_PATH = "";
     
     public static final String BOT_TOKEN_FILENAME = FILES_PATH + "bot_token.txt";
     public static final String LAST_ID_FILENAME = FILES_PATH + "last_id.txt";
