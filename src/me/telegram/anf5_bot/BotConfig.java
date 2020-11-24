9cad813f0bb6ff79366ea9c82e749031d675a48a
Helltar <helltar.live@gmail.com>
2020-11-24 15:54:40 +0200

    read channel_name from file

--- src/me/telegram/anf5_bot/BotConfig.java
+++ src/me/telegram/anf5_bot/BotConfig.java
@@ -2,10 +2,11 @@ package me.telegram.anf5_bot;
 
 public class BotConfig {
     
-    public static final String CHANNEL_NAME = "@annimon_live";
-    
-    private static final String FILES_PATH = "";
+    public static String CHANNEL_NAME = "@anf5_dev"; // channel.txt
+
+    private static final String FILES_PATH = "/storage/emulated/0/AppProjects/anf5_bot/";
     
     public static final String BOT_TOKEN_FILENAME = FILES_PATH + "bot_token.txt";
     public static final String LAST_ID_FILENAME = FILES_PATH + "last_id.txt";
+    public static final String CHANNEL_FILENAME = FILES_PATH + "channel.txt";
 }
--- src/me/telegram/anf5_bot/Main.java
+++ src/me/telegram/anf5_bot/Main.java
@@ -3,6 +3,10 @@ package me.telegram.anf5_bot;
 public class Main {
 
 	public static void main(String[] args) {
+        
+        BotConfig.CHANNEL_NAME = 
+            Utils.getStringFromFile(BotConfig.CHANNEL_FILENAME);
+
         Logger.add("bot start ...");
 
         new Anf5Bot(
